e04bc85d9c1fab8880015ff92b8ce5c989e6ddb8
Amauri Santos <amauri.sanntos@gmail.com>
2022-09-11 02:12:43 -0300

    Send notification

--- frontend/src/components/NotificationButTon/index.tsx
+++ frontend/src/components/NotificationButTon/index.tsx
@@ -1,9 +1,21 @@
 import icon from '../../assets/img/notification-icon.svg';
 import './styles.css';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type props = {
+    saleId: number;
+}
+
+function hendleClick(id :number) {
+    axios(`${BASE_URL}/sales/${id}/notification`).then(Response => {
+        console.log("SUCESSO");
+    })
+}
+
+function NotificationButton( {saleId} : props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => hendleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
