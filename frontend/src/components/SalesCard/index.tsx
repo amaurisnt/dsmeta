b2200b7e711f3ee0a7160072f2d4bf650733c564
Amauri Santos <amauri.sanntos@gmail.com>
2022-09-11 01:37:07 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,10 +19,14 @@ function SalesCard() {
 
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`).then(Response => {
+
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(Response => {
             setSales(Response.data.content);
         });
-    }, []);
+    }, [minDate, maxDate]);
 
 
     return (
